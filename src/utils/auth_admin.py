62bb93bf802e1430b6a441fbf7840b83f8e0cf8c
Igor228337 <Dj.igo@yandex.ru>
2025-04-04 20:46:10 +0300

    clear

--- src/utils/auth_admin.py
+++ src/utils/auth_admin.py
@@ -12,23 +12,19 @@ class BasicAuthBackend(AuthenticationBackend):
         username = form.get("username")
         password = form.get("password")
 
-        # Проверка логина и пароля
         correct_username = secrets.compare_digest(username, enviroment.LOGIN_ADMIN)
         correct_password = secrets.compare_digest(password, enviroment.PASSWORD_ADMIN)
 
         if not (correct_username and correct_password):
             raise HTTPNotVerifyDataError()
 
-        # Сохраняем пользователя в сессии
         request.session.update({"username": username})
         return True
 
     async def logout(self, request: Request) -> bool:
-        # Очищаем сессию
         request.session.clear()
         return True
 
     async def authenticate(self, request: Request) -> bool:
-        # Проверяем, авторизован ли пользователь
         username = request.session.get("username")
         return username == enviroment.LOGIN_ADMIN
